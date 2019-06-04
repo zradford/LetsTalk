5d1a034aa86f57c14d11f14eb46aa172e631c2b3
zradford <rad12009@byui.edu>
2019-06-03 21:33:26 -0600

    set up mvc i think. created welcome page

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const http = require('http')
 const express = require('express')
+const app = express()
+
+
+//static files
+app.use(express.static('public'))
+
 const port = process.env.PORT || 3000
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    console.log('server received request.')
-    response.end("Hello Katie!");
-});
-
-server.listen(port, err  => {
+// start app
+app.listen(port, err  => {
    if(err){
       console.log(err);
    } else {
       console.log(`Server listening on port ${port}`);
    }
 });
+
+// basic routes
+app.use('/', 'welcome.html')
