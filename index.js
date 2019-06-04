8aa8ccbb3c35bdae00e8711a3ab29b0abb026ec6
zradford <rad12009@byui.edu>
2019-06-04 15:24:42 -0600

    debugging

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const app = express()
 //static files
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use("/", function(req, res, next){
-   console.log(req.url)
-   next()
-})
-
 const port = process.env.PORT || 3000
 
 // start app
@@ -28,6 +23,8 @@ app.listen(port, err  => {
 
 // basic routes
 app.get("/", function(req, res) {
+   console.log(__dirname)
+   
    res
       .status(200)
       .sendFile(path.join(__dirname +'welcome.html'))
