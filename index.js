7cc7bc55905e046dcdf301d0e9a34bd9dd36d411
zradford <rad12009@byui.edu>
2019-06-12 17:12:54 -0600

    starting new app

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ appInsights.start();
 const http = require('http')
 const path = require('path')
 const express = require('express')
-const hbs = require('express-handlebars')
 const app = express()
-const port = process.env.PORT || 3000
+const hbs = require('express-handlebars')
+
+const PORT = process.env.PORT || 3000
 
 
 // I am using handlebars
@@ -19,15 +20,15 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
 // start app
-app.listen(port)
+app.listen(PORT)
 
 // generic middleware that tells me what is being accessed
-app.all('/', function(req, res, next){
+app.all('/', (req, res, next) => {
    console.log("Requesting: " + req.url)
    next()
 })
 
 // basic routes
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
    res.render('index', {title: 'Hey', message: 'If you see this, txt me an emoji'})
 })
