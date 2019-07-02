c88a9bdaedc895352e4070b1911a68d294edae1f
zradford <rad12009@byui.edu>
2019-07-02 15:08:59 -0600

    added the code for the db

---
+++ database.sql
@@ -0,0 +1,35 @@
+CREATE TABLE user (
+   user_id    SERIAL      PRIMARY KEY,
+   first_name VARCHAR(80)  NOT NULL,
+   last_name  VARCHAR(80)  NOT NULL,
+   email      VARCHAR(250) NOT NULL,
+   hash_pass  VARCHAR(250) NOT NULL,
+   region  VARCHAR(100) NOT NULL 
+)
+
+CREATE TABLE topic (
+   topic_id    SERIAL PRIMARY KEY,
+   creator_id  INTEGER REFERENCES user(user_id),
+   topic_name  TEXT NOT NULL,
+   description TEXT NOT NULL,
+   region      VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE comment (
+   comment_id SERIAL PRIMARY KEY,
+   creator_id INTEGER REFERENCES user(user_id),
+   topic_id   INTEGER REFERENCES topic(topic_id),
+   content    TEXT NOT NULL
+)
+
+CREATE TABLE reply (
+   reply_id SERIAL PRIMARY KEY,
+   creator_id INTEGER REFERENCES user(user_id),
+   reply_to_id INTEGER REFERENCES user(user_id),
+   content    TEXT NOT NULL
+)
+
+CREATE TABLE region (
+   region_id SERIAL PRIMARY KEY,
+   region_name VARCHAR(255)
+)
