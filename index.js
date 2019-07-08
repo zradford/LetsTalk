499bc394958b94624333774c70866f046bc4349d
zradford <rad12009@byui.edu>
2019-07-08 16:39:00 -0600

    fix erros

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/loggingin', /*urlencodedparser,*/ (req, res) => {
    // send the info from the login form to the database for verification
    //instead of req.body
 
-   login.login(NULL, req.body)
+   login.login(null, req.body)
    res.render('user/homepage', {css: '<link rel="stylesheet" href="stylesheets/home.css"'})
 })
 app.post('/loggingin', /*urlencodedparser,*/ (req, res) => {
