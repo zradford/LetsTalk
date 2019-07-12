0ab712f78eea6455d74b570d9b17854bdc545262
zradford <rad12009@byui.edu>
2019-07-12 15:06:21 -0600

    inserting error messages

--- controllers/register.js
+++ controllers/register.js
@@ -2,6 +2,7 @@ const db = require('../models/database')
 const hasher = require('../models/hash')
 
 function register(err, data) {
+   console.log("in register.js: register()")
    if(err) {
       console.log(err) 
       throw err;
