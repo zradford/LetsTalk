65f07ce67377f5069977e1fd8c23f6e329f8ac8a
zradford <rad12009@byui.edu>
2019-07-12 12:20:33 -0600

    fixing an oops

--- controllers/register.js
+++ controllers/register.js
@@ -3,7 +3,7 @@ const db = require('../models/database')
 
 function register(err, data) {
    if(err) console.log(err)
-   db.register()
+   db.register(data.first, data.last, data.email, data.username, data.password)
 }
 
 module.exports = {
