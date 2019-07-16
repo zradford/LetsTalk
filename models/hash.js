0bf9b5c16165bb4ba6e3b5c935c7e0d26b9951fa
zradford <rad12009@byui.edu>
2019-07-15 19:56:17 -0600

    quick test

--- models/hash.js
+++ models/hash.js
@@ -19,7 +19,6 @@ function checker(plaintext, hash) {
    return new Promise((resolve, reject) => {
       // plaintext = "" + plaintext;
       // hash = "" + hash;
-      console.log(hash)
       bcrypt.compare(plaintext, hash)
          .then(res => resolve(res))
          .catch(e => reject(e));
