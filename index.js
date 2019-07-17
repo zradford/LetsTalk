7fe2d3f6af1317d66fd7498b871f15d532846db6
zradford <rad12009@byui.edu>
2019-07-17 14:28:05 -0600

    fix typo

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.user(session({
+app.use(session({
    secret: process.env.SESSION_SECRET,
    resave: false,
    saveUninitialized: true
