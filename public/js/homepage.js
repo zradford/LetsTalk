141c6c44b7a62ac60cd18a47e155aca5f6643410
zradford <rad12009@byui.edu>
2019-07-20 13:55:30 -0600

    quick fix?

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -3,16 +3,20 @@ function postNewTopic(){
    let region = null
    let desc = document.getElementById('desc').value
 
-   let reqBody = new FormData();
-   reqBody.append('topic_name', topic_name)
-   reqBody.append('region', region)
-   reqBody.append('desc', desc)
+   let reqBody = {
+      topic_name: topic_name,
+      region : region,
+      desc: desc
+   }
 
-   
+
    fetch('/api/topics', {
       method: 'POST',
       credentials: "include",
-      body: reqBody
+      body: JSON.stringify(reqBody),
+      headers : {
+         'Content-Type': 'application/json'
+      }
    })
    .then(response => {
     return response.json();
