c8df56727b46bb67206108cb2fd605d80a9ccf94
zradford <rad12009@byui.edu>
2019-07-20 16:50:59 -0600

    testing

--- index.js
+++ index.js
@@ -197,7 +197,11 @@ app.post('/api/topics', (req,res) => {
 
 app.post('/api/topics', (req,res) => { 
    database.setUserRegions(req.body.region_1, req.body.region_2, req.body.region_3, req.user.username)
-      .then(data => res.send(data))
+      .then(data => {
+         console.log(data)
+         res.send(data)
+
+      })
       .catch(e => console.log(e))
 })
 
